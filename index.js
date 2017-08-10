99c186141a1f58495a07f3724cbbdc36eeda5480
Олег Бублик <bublik_oleg@hotmail.com>
2017-08-10 03:03:32 +0300

    another one

--- index.js
+++ index.js
@@ -14,15 +14,17 @@ var s = http.createServer(function (req, res) {
 	let ext = name.substr(name.lastIndexOf('.') + 1);
 	
 	if(ext == 'db') {
+		
 		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 			client.query('select * from test_table', function (err, q) {
 				if (err) {
 					console.log(err.stack)
 				} else {
 					res.writeHead(200, {'Content-Type': 'text/html'});
-					res.end(q.rows[0]);
+					res.end(q.rows[0]||'123');
 				}
 			});
+			client.end();
 		});
 	}
 	else {
