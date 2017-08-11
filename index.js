a981afe8998bd4b264f3701516ff4a3d23004c4c
Олег Бублик <bublik_oleg@hotmail.com>
2017-08-11 08:12:24 +0300

    some result

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const fs = require('fs');
 const pg = require('pg');
 const qs = require('querystring');
 
-let tables = [];
-if (process.env.DATABASE_URL) {
-	const pool = new pg.Pool({
-		connectionString: process.env.DATABASE_URL,
+let local_database_url = undefined;
+
+/*
+const pool = new pg.Pool({
+		connectionString: process.env.DATABASE_URL || local_database_url,
 	});
+let tables = [];
+if (process.env.DATABASE_URL || local_database_url) {
+	
 	
 	pool.query("select table_name from information_schema.tables where table_schema like 'public'", (error, result) => {
 		if (!error) {
@@ -16,10 +20,10 @@ if (process.env.DATABASE_URL) {
 				tables.push(table_list[table].table_name);	
 			}
 		}
-		console.log('table list: ' + JSON.stringify(tables));
 		pool.end();
 	});
 }	
+*/
 
 const s = http.createServer(function (req, res) {
 	console.log('☻ URL: ' + req.url);
@@ -30,96 +34,143 @@ const s = http.createServer(function (req, res) {
 	}
 	let ext = name.substr(name.lastIndexOf('.') + 1);
 
-	if (ext == 'db') {
-		if (req.method == 'GET') {
-			
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write('<form method="post"><div><label for="label">Query:</label><input type="txt" id="label" name="query" value="" /></div><input type="submit" /></form>');
-			res.end();
+	if (req.method == 'GET') {
+
+		let sup_ext = {
+			'html': 'text/html',
+			'css': 'text/css',
+			'js': 'text/javascript',
+			'ico': 'image/x-icon',
+			'db': 'text/html'
+		};
+		
+		if (name == 'simple_query.db') {
+			simple_query(req, res);
 		}
-		else if (req.method == 'POST') {
-			
+		else {
+			if (ext == 'db') {
+				name = 'database.html';
+			}
+			if (sup_ext[ext]) {
+				fs.readFile(name, function (err, data) {
+					res.writeHead(200, { 'Content-Type': sup_ext[ext] });
+					res.end(data, 'utf-8');
+				});
+			}
+			else {
+				console.log('Unknown extention: ' + ext);
+				res.writeHead(404, 'not found', { 'Content-Type': 'text/html' });
+				res.end('boo');
+			}
+		}	
+
+	} else if (req.method == 'POST') {
+
+		if (ext == 'test') {
+
 			let x = '';
 			req.on('data', function (data) {
 				x += data;
 			});
-			req.on('end', function (data) {
-				console.log(qs.parse(x));
-				if (process.env.DATABASE_URL) {
+			req.on('end', function () {
+				x = JSON.parse(x);
+			
+				const pool = new pg.Pool({
+					connectionString: process.env.DATABASE_URL || local_database_url,
+				});
+				pool.query(`create table if not exists ${x.login}_actions \
+					(github_user text, target text, time text)`, function (error, result) {
+				
+					/*
+					if (!tables.indexOf(x.login))
+						console.log(`successfully created table ${x.login}_actions`);
+					else
+						console.log(`table ${x.login}_actions already exists`);
+					*/
 
-					res.writeHead(200, { 'Content-Type': 'text/html' });
-
-					const pool = new pg.Pool({
-						connectionString: process.env.DATABASE_URL,
-					});
-					pool.query(qs.parse(x).query, (err, r) => {
-						//res.write('<p>' + tables + JSON.stringify(tables) + '</p>');
-						res.end(`<p>${JSON.stringify(err)}, ${JSON.stringify(r)}</p>`);
+					pool.query(`insert into ${x.login}_actions values \
+						('${x.login}', '${x.target}', '${x.time}')`, function (error, result) {
+						
 						pool.end();
 					});
-					
-				}
-				else {
-
-				
-					res.statusCode = 200;
-					res.end();
-
-				}
+					pool.end();
+				});
+			
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				res.end(x + ' recieved!');
 			});
 
+		} else if (ext == 'db') {
+			if (name == 'simple_query.db') {
+				simple_query(req, res);
+			}
+			else {
+				
+				req.on('data', function () {
+					// need!				
+				});
+			
+				req.on('end', function () {
+					const pool = new pg.Pool({
+						connectionString: process.env.DATABASE_URL || local_database_url,
+					});
+					let user = name.substring(0, name.indexOf('.'));
+					pool.query(`select * from ${user}`, function (error, result) {
+						res.writeHead(200, { 'Content-Type': 'application/json' });
+						res.end(JSON.stringify(result, null, 2));
+						pool.end();
+					})
+				
+				});
+			}	
 		}
-	} else if (ext == 'test') {
+		
+	} else {
+		res.statusCode = 405;
+		res.end('not so fast');
+	}
+
+});
+
+s.listen(process.env.PORT || 8080);
+
+
+// this function makes possible to input queries to database on page /simple_query.db
+// just executes queries and displays result as text
+function simple_query(req, res) {
+	if (req.method == 'GET') {
+			
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write('<form method="post"><div><label for="label">Query:</label>\
+			<input type="txt" id="label" name="query" value="" /></div><input type="submit" /></form>');
+		res.end();
+	}
+	else if (req.method == 'POST') {
 
 		let x = '';
 		req.on('data', function (data) {
 			x += data;
 		});
-		req.on('end', function () {
-			x = JSON.parse(x);
-			
-			const pool = new pg.Pool({
-				connectionString: process.env.DATABASE_URL,
-			});
-			pool.query(`create table if not exists ${x.login}_actions (github_user text, target text, time text)`, function (error, result) {
-				const pool2 = new pg.Pool({
-					connectionString: process.env.DATABASE_URL,
+		req.on('end', function (data) {
+			if (process.env.DATABASE_URL || local_database_url) {
+
+				res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+				const pool = new pg.Pool({
+					connectionString: process.env.DATABASE_URL || local_database_url,
 				});
-				if (!tables.indexOf(x.login))
-					console.log(`successfully created table ${x.login}_actions`);
-				else
-					console.log(`table ${x.login}_actions already exists`);
-				pool2.query(`insert into ${x.login}_actions values ('${x.login}', '${x.target}', '${x.time}')`, function (error, result) {
-					console.log(JSON.stringify(error));
-					console.log(JSON.stringify(result));
-					console.log(`successfully added values into table ${x.login}_actions`);
-					pool2.end();
-				})
-				pool.end();
-			})
-			
-			console.log('to_db: ' + JSON.stringify(x));
-			res.writeHead(200, { 'Content-Type': 'application/json' });
-			res.end(x + ' recieved!');
+				pool.query(qs.parse(x).query, (err, r) => {
+					res.end(`error: ${JSON.stringify(err)}\nquery: ${JSON.stringify(r, null, 2)}`);
+					pool.end();
+				});
+					
+			}
+			else {
+
+				res.statusCode = 200;
+				res.end();
+
+			}
 		});
-
-	} else {
-
-		let sup_ext = { 'html': 'text/html', 'css': 'text/css', 'js': 'text/javascript', 'ico': 'image/x-icon' };
-
-		if (sup_ext[ext]) {
-			fs.readFile(name, function (err, data) {
-				res.writeHead(200, { 'Content-Type': sup_ext[ext] });
-				res.end(data, 'utf-8');
-			});
-		}
-		else {
-			console.log('Unknown extention: ' + ext);
-			res.writeHead(404, 'not found', { 'Content-Type': 'text/html' });
-			res.end('boo');
-		}
-
 	}
-});
-
-s.listen(process.env.PORT || 8080);
+}
--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-let r = new XMLHttpRequest(),
-    credentials;
+let graphqlRequest = new XMLHttpRequest(),
+    user_credentials;
 
-
+    
+// this function simply changes "status" string in #status-container element
 function set_status(st) {
     var st_cont = document.getElementById('status-container');
-    //.children[0].innerHTML = st;
     if(st == 'init') {
-        st_cont.style.boxShadow = '0 4px 10px 0 rgba(255, 152, 0, 0.2), \
-                                    0 4px 20px 0 rgba(255, 152, 0, 0.19)';
+        st_cont.style.boxShadow = '0 4px 10px 0 rgba(255, 152, 0, 0.5), \
+                                    0 4px 20px 0 rgba(255, 152, 0, 0.35)';
         st_cont.children[0].innerHTML = 'pls, log in';
     } else if(st == 'no_login') {
-        st_cont.style.boxShadow = '0 4px 10px 0 rgba(244, 67, 54, 0.2), \
-                                    0 4px 20px 0 rgba(244, 67, 54, 0.19)';
+        st_cont.style.boxShadow = '0 4px 10px 0 rgba(244, 67, 54, 0.5), \
+                                    0 4px 20px 0 rgba(244, 67, 54, 0.35)';
         st_cont.children[0].innerHTML = 'no login';
     } else if(st == 'no_password') {
-        st_cont.style.boxShadow = '0 4px 10px 0 rgba(244, 67, 54, 0.2), \
-                                    0 4px 20px 0 rgba(244, 67, 54, 0.19)';
+        st_cont.style.boxShadow = '0 4px 10px 0 rgba(244, 67, 54, 0.5), \
+                                    0 4px 20px 0 rgba(244, 67, 54, 0.35)';
         st_cont.children[0].innerHTML = 'no password';
     } else if(st == 'bad_credentials') {
-        st_cont.style.boxShadow = '0 4px 10px 0 rgba(244, 67, 54, 0.2), \
-                                    0 4px 20px 0 rgba(244, 67, 54, 0.19)';
+        st_cont.style.boxShadow = '0 4px 10px 0 rgba(244, 67, 54, 0.5), \
+                                    0 4px 20px 0 rgba(244, 67, 54, 0.35)';
         st_cont.children[0].innerHTML = 'bad credentials';
     } else if(st == 'no_such_user') {
-        st_cont.style.boxShadow = '0 4px 10px 0 rgba(255, 152, 0, 0.2), \
-                                    0 4px 20px 0 rgba(255, 152, 0, 0.19)';
+        st_cont.style.boxShadow = '0 4px 10px 0 rgba(255, 152, 0, 0.5), \
+                                    0 4px 20px 0 rgba(255, 152, 0, 0.35)';
         st_cont.children[0].innerHTML = 'no such user';
     } else if(st == 'no_user') {
-        st_cont.style.boxShadow = '0 4px 10px 0 rgba(255, 152, 0, 0.2), \
-                                    0 4px 20px 0 rgba(255, 152, 0, 0.19)';
+        st_cont.style.boxShadow = '0 4px 10px 0 rgba(255, 152, 0, 0.5), \
+                                    0 4px 20px 0 rgba(255, 152, 0, 0.35)';
         st_cont.children[0].innerHTML = 'no user ';
     } else if(st == 'found') {
         st_cont.style.boxShadow = '0 4px 10px 0 rgba(76, 175, 80, 0.5), \
@@ -40,6 +40,34 @@ function set_status(st) {
     }
 }
 
+// this function sends request to github API and validates user's credentials
+function check_user_credentials(login, password) {
+    let req = new XMLHttpRequest();
+    req.open('POST', 'https://api.github.com/graphql');
+    req.addEventListener('loadend', function () {
+        //console.log(req.response);
+        if(JSON.parse(req.response).data){
+            user_credentials = btoa(login + ':' + password);
+            set_status('good');
+            document.getElementById('search-button').disabled = false;
+            document.getElementById('form-container').style.transform = 'rotateY(180deg)';
+            document.getElementById('loggined-as').innerHTML = `Logged in as: \
+                <span id="loggined">${login}</span>`;
+            document.getElementById('database-viewer').innerHTML = `view your \
+                database <a href="/${login}_actions.db" target="_blank" \
+                id="database-link">here</a>`;
+        } else {
+            set_status('bad_credentials');
+        }
+
+    });
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('Accept', 'application/json');
+    req.setRequestHeader('Authorization', `Basic ${btoa(login + ':' + password)}`);
+    req.send(JSON.stringify({"query": "query { viewer { login } }"}));
+}
+
+// here we attached custom "submit" event to #form-container
 document.getElementById('form-container').children[0].onsubmit = function (ev) {
     ev.preventDefault();
     let login = document.getElementById('log').value,
@@ -53,42 +81,104 @@ document.getElementById('form-container').children[0].onsubmit = function (ev) {
     } else if(!password) {
         set_status('no_password');
     } else {
-        check_credentials(login, password);
+        check_user_credentials(login, password);
     }
 }
 
+// this function sends request to github API and gets information about 
+// user's repositories and issues
+function graphql_query (login, n) {
+    graphqlRequest.open('POST', 'https://api.github.com/graphql');
+
+    graphqlRequest.addEventListener('loadend', add_content);
+
+    graphqlRequest.setRequestHeader('Content-Type', 'application/json');
+    graphqlRequest.setRequestHeader('Accept', 'application/json');
+    graphqlRequest.setRequestHeader('Authorization', `Basic ${user_credentials}`);
+
+    graphqlRequest.send(JSON.stringify({ 
+        "query":
+        "query($login:String!, $nOfRepos:Int!) { \
+            user(login: $login) { \
+                login \
+                repositories(first: $nOfRepos) { \
+                    totalCount \
+                    nodes { \
+                        name \
+                        url \
+                        issues(first: $nOfRepos) { \
+                            nodes { \
+                                title \
+                                url \
+                            } \
+                        } \
+                    } \
+                } \
+            } \
+        }", 
+        "variables": {
+            "login": login, 
+            "nOfRepos": n
+        } 
+    }));
+}
+
+// this function sends the request to server to insert user's search action
+// into user's_action table in database
+function postgresql_query(target) {
+    let req = new XMLHttpRequest();
+    req.open('POST', '/abc.test');
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('Accept', 'application/json');
+    req.addEventListener('loadend', function () {
+        console.log(this.response);
+    });
+    req.send(JSON.stringify({
+        'login': document.getElementById('loggined').innerText,
+        'target': target,
+        'time': (new Date()).toLocaleTimeString()
+    }));
+}
+
+// this function empties previous github API request result and initiate new one,
+// also pushes user's search requests to database
+function action() {
+    let gh_user = document.getElementById('github-user');
+    if (gh_user.value) {
+        postgresql_query(gh_user.value);
+        gh_user.classList.toggle('wow');
+        document.getElementById('search-button').style.display = 'none';
+        let l = document.getElementById('item-container');
+        let c = l.children;
+        while (c.length) {
+            l.removeChild(c[0]);
+        }
+        graphql_query(gh_user.value, 15);
+    } else {
+        set_status('no_user');
+    }
+}
+
+// resets user's log in 
 document.getElementById('acc-changer').onclick = function (ev) {
     document.getElementById('search-button').disabled = true;
     document.getElementById('form-container').style.transform = 'rotateY(0deg)';
     document.getElementById('log').value = '';
     document.getElementById('psw').value = '';
-    credentials = undefined;
+    user_credentials = undefined;
     set_status('init');
 }
 
-function check_credentials(login, password) {
-    let req = new XMLHttpRequest();
-    req.open('POST', 'https://api.github.com/graphql');
-    req.addEventListener('loadend', function () {
-        //console.log(req.response);
-        if(JSON.parse(req.response).data){
-            credentials = btoa(login + ':' + password);
-            set_status('good');
-            document.getElementById('search-button').disabled = false;
-            document.getElementById('form-container').style.transform = 'rotateY(180deg)';
-            document.getElementById('loggined-as').innerHTML = `Logged in as: <span id="loggined">${login}</span>`;
-        } else {
-            set_status('bad_credentials');
-        }
-
-    });
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.setRequestHeader('Accept', 'application/json');
-    req.setRequestHeader('Authorization', `Basic ${btoa(login + ':' + password)}`);
-    req.send(JSON.stringify({"query": "query { viewer { login } }"}));
-}
-
-function ld (e) {
+// this function  github API request and add content to #item-container
+// by the following schema
+// add row ('div' with class .item)
+//   add subrow for repository ('div' with class .repohld)
+//     add tooltip to repository ('span' with class .tooltipster)
+//     add href for repository name ('a')
+//   add subrow for issues ('div' with class .isshld)
+//     add (n) issue(s) ('div' with class .iss)
+//       add href to issue ('a')
+function add_content (e) {
 
     let resp = JSON.parse(this.response);
     //console.log(JSON.stringify(resp, null, 2));
@@ -109,6 +199,7 @@ function ld (e) {
             let tooltip = document.createElement('span');
             tooltip.classList.add('tooltipster');
             tooltip.innerHTML = repository.url;
+
             repoHolder.appendChild(tooltip);
             let repoContent = document.createElement('a');
             repoContent.innerHTML = repository.name;
@@ -150,87 +241,19 @@ function ld (e) {
                                     parseInt(getComputedStyle(tooltip).width, 10)) / 2;
             
             addEvs();
-            //li.addEventListener('click', () => alert (el.name));
         }
     } else {
         set_status('no_such_user');
     }
 }
 
-function some_testing(target) {
-    let req = new XMLHttpRequest();
-    req.open('POST', '/abc.test');
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.setRequestHeader('Accept', 'application/json');
-    req.addEventListener('loadend', function () {
-        console.log(this.response);
-    });
-    req.send(JSON.stringify({
-        'login': document.getElementById('loggined').innerText,
-        'target': target,
-        'time': (new Date()).toLocaleTimeString()
-    }));
-}
-
-function action() {
-    let gh_user = document.getElementById('github-user');
-    if (gh_user.value) {
-        some_testing(gh_user.value);
-        gh_user.classList.toggle('wow');
-        document.getElementById('search-button').style.display = 'none';
-        let l = document.getElementById('item-container');
-        let c = l.children;
-        while (c.length) {
-            l.removeChild(c[0]);
-        }
-        update(gh_user.value, 15);
-    } else {
-        set_status('no_user');
-    }
-}
-
+// just prettifies user request
 document.getElementById('github-user').onfocus = function (event) {
     document.getElementById('github-user').classList.remove('wow');
     document.getElementById('search-button').style.display = 'inline-block';
 }
 
-function update (login, n) {
-    r.open('POST', 'https://api.github.com/graphql');
-
-    //r.addEventListener('progress', pr);
-    r.addEventListener('loadend', ld);
-
-    r.setRequestHeader('Content-Type', 'application/json');
-    r.setRequestHeader('Accept', 'application/json');
-    r.setRequestHeader('Authorization', `Basic ${credentials}`);
-
-    r.send(JSON.stringify({ 
-        "query":
-        "query($login:String!, $nOfRepos:Int!) { \
-            user(login: $login) { \
-                login \
-                repositories(first: $nOfRepos) { \
-                    totalCount \
-                    nodes { \
-                        name \
-                        url \
-                        issues(first: $nOfRepos) { \
-                            nodes { \
-                                title \
-                                url \
-                            } \
-                        } \
-                    } \
-                } \
-            } \
-        }", 
-        "variables": {
-            "login": login, 
-            "nOfRepos": n
-        } 
-    }));
-}
-
+// adds sliding effect to issues container #isshld
 function addEvs () {
     let targets = document.getElementsByClassName('repohld');
     for ( let i = 0; i < targets.length; i++ ) {
